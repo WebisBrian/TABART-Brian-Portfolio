17effc54a59f1e4187a8d6009ee9dc09a05a06b7
WebisBrian <brian.t.github@gmail.com>
2026-03-10 10:07:55 +0100

    Feat: add ModeToggle and temporary h1 / p

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
+import { ModeToggle } from "@/components/theme-toggle";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div >
-      <main>Coucou</main>
+      <main>
+        <h1>Brian TABART</h1>
+        <p>Développeur backend</p>
+        <ModeToggle />
+      </main>
     </div>
   );
 }
