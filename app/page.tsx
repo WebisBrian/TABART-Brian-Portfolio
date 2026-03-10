65fdd201a64bd680e0041fa3f19a544674344ace
WebisBrian <brian.t.github@gmail.com>
2026-03-10 10:54:01 +0100

    Feat: temporary design with heading section

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,21 @@
+import { Section } from "@/components/layout/section";
+import { SectionHeading } from "@/components/layout/section-heading";
+import { SiteContainer } from "@/components/layout/site-container";
 import { ModeToggle } from "@/components/theme-toggle";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <div >
-      <main>
-        <h1>Brian TABART</h1>
-        <p>Développeur backend</p>
+      <main className="min-h-screen bg-background text-foreground">
+        <Section>
+          <SiteContainer>
+            <SectionHeading
+              eyebrow="Portfolio"
+              title="Brian Tabart"
+              description="Bienvenue sur mon portfolio ! Je suis développeur backend Java / Spring Boot et j'ai conçu ce site avec Next.js, Tailwind CSS et shadcn/ui."
+              />
+          </SiteContainer>          
+        </Section>
         <ModeToggle />
       </main>
     </div>
