2461d2c2f833eea1bbd5cd23b17c39036dd11685
WebisBrian <brian.t.github@gmail.com>
2026-03-16 11:34:41 +0100

    Feat: implements reading-section in main page

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import { JourneySection } from "@/components/sections/journey-section";
 import { Separator } from "@/components/ui/separator";
 import { StackSection } from "@/components/sections/stack-section";
 import { ProjectsSection } from "@/components/sections/projects-session";
+import { ReadingSection } from "@/components/sections/reading-section";
 
 export default function Home() {
   return (
@@ -18,6 +19,8 @@ export default function Home() {
         <StackSection />
         <Separator />
         <ProjectsSection />
+        <Separator />
+        <ReadingSection />
       </main>
     </>
   );
