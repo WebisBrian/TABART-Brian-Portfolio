82eefd47697da8acbab5a8f0e0a8d29ea275fe68
WebisBrian <brian.t.github@gmail.com>
2026-03-17 10:55:30 +0100

    Refacto: remove useClient and use DRY

--- src/components/layout/navbar1.tsx
+++ src/components/layout/navbar1.tsx
@@ -1,91 +1,48 @@
-"use client";
-
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 import Image from "next/image";
 import Link from "next/link";
 
-import { cn } from "@/lib/utils";
-
 import { Button } from "@/components/ui/button";
 import { ModeToggle } from "../ui/theme-toggle";
 
-interface Navbar1Props {
-  className?: string;
-  logo?: {
-    url: string;
-    src: string;
-    alt: string;
-    title: string;
-    className?: string;
-  };
-  social?: {
-    github: {
-      title: string;
-      url: string;
-    };
-    linkedin: {
-      title: string;
-      url: string;
-    };
-  };
-}
-
-const Navbar1 = ({
-  logo = {
-    url: "/",
-    src: "/logo/logo-whereisbrian.svg",
-    alt: "logo du portfolio de Brian Tabart",
-    title: "Brian Tabart - Développeur Java",
-  },
-  social = {
-    github: {
-      title: "GitHub",
-      url: "https://github.com/WebisBrian",
-    },
-    linkedin: {
-      title: "LinkedIn",
-      url: "https://linkedin.com/in/brian-tabart",
-    },
-  },
-  className,
-}: Navbar1Props) => {
+const Navbar1 = () => {
   return (
-    <nav className={cn("py-4", className)}>
+    <nav className="py-4">
       <div className="flex w-full items-center justify-between">
-        <Link href={logo.url} className="flex items-center gap-4">
+        <Link href="/" className="flex items-center gap-4">
           <Image
-            src={logo.src}
+            src="/logo/logo-whereisbrian.svg"
             width={32}
             height={32}
             className="dark:invert"
-            alt={logo.alt}
+            alt="logo du portfolio de Brian Tabart"
           />
           <span className="hidden sm:inline text-lg font-semibold tracking-tighter">
-            {logo.title}
+            Brian Tabart - Développeur Java
           </span>
         </Link>
 
         <div className="flex items-center gap-4">
           <Button variant="ghost" size="icon" asChild>
-            <Link
-              href={social.github.url}
+            <a
+              href="https://github.com/WebisBrian"
               target="_blank"
               rel="noreferrer"
-              aria-label={social.github.title}
+              aria-label="GitHub"
             >
               <FaGithub className="h-5 w-5" aria-hidden="true" />
-            </Link>
+            </a>
           </Button>
 
           <Button variant="ghost" size="icon" asChild>
-            <Link
-              href={social.linkedin.url}
+            <a
+              href="https://linkedin.com/in/brian-tabart"
               target="_blank"
               rel="noreferrer"
-              aria-label={social.linkedin.title}
+              aria-label="LinkedIn"
             >
               <FaLinkedin className="h-5 w-5" aria-hidden="true" />
-            </Link>
+            </a>
           </Button>
 
           <ModeToggle />
