16338f01e81b443e0e208323d018fcbaeab1896f
WebisBrian <brian.t.github@gmail.com>
2026-03-17 16:51:01 +0100

    Feat: add scroll-mt-20 class

--- src/components/layout/section.tsx
+++ src/components/layout/section.tsx
@@ -8,7 +8,7 @@ type SectionProps = {
 
 export function Section({ id, children, className }: SectionProps) {
   return (
-    <section id={id} className={cn("py-20 sm:py-30", className)}>
+    <section id={id} className={cn("py-20 sm:py-30 scroll-mt-20", className)}>
       {children}
     </section>
   )
