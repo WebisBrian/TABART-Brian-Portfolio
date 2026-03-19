dc4729e468294298e402c668da9f68ec14731d0e
WebisBrian <brian.t.github@gmail.com>
2026-03-19 10:27:24 +0100

    Feat: French translation

--- src/components/ui/theme-toggle.tsx
+++ src/components/ui/theme-toggle.tsx
@@ -20,7 +20,7 @@ export function ModeToggle() {
         <Button variant="outline" size="icon">
           <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
-          <span className="sr-only">Toggle theme</span>
+          <span className="sr-only">Changer le thème</span>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
@@ -28,13 +28,13 @@ export function ModeToggle() {
           className="cursor-pointer"
           onClick={() => setTheme("light")}
         >
-          Light
+          Clair
         </DropdownMenuItem>
         <DropdownMenuItem
           className="cursor-pointer"
           onClick={() => setTheme("dark")}
         >
-          Dark
+          Sombre
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
